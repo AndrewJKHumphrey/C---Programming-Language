f0413e19aa8f2d3dc4c89a521bf1155f9722100d
AndrewJKHumphrey <andrew.jk.humphrey@gmail.com>
2023-12-04 14:25:26 -0500

    Cleaned up build files

---
+++ C++ Primer/VS Code Clean C++.ps1
@@ -0,0 +1,14 @@
+﻿$chapter = 4;
+
+$path = "C:\Projects\Current Projects\C++ Programming Language\C++ Primer\Chapter " + $chapter;
+
+$files =  Get-Childitem -Recurse $path;
+
+foreach($file in $files) {
+    if($file.name -match ".exe" -or $file.name -match ".obj" -or $file.name -match ".ilk" -or $file.name -match ".pdb") {
+        Write-Host "Removing $($file)";
+        Remove-Item $file.FullName;
+        
+    }
+
+}
