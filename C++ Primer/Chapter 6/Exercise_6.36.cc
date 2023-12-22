29797b58cbd202b7a8605e483a4012a982108b09
AndrewJKHumphrey <andrew.jk.humphrey@gmail.com>
2023-12-22 17:26:46 -0500

    Daily Check In

--- C++ Primer/Chapter 6/Exercise_6.36.cc
+++ C++ Primer/Chapter 6/Exercise_6.36.cc
@@ -8,20 +8,19 @@ of ten strings, without using either a trailing return, decltype, or a type alia
 #include <string> 
 using std::cout, std::cin, std::endl, std::string,  std::vector, std::begin, std::end, std::runtime_error;
 
-//Type (function(parameter_list))[dimension]
-string (&hardWay(string s)) [10] {
 
-}
-
-
-string cold[] = {"snow", "ice", "sleet"};
-string hot[] = {"sand", "desert", "sandstorm"};
-decltype(cold) *arrPtr(string s)
-{
-    return  s == *(&cold[1]) ? &cold : &hot; 
-}
+//Type (*function(parameter_list))[dimension]
+string (*hardWay(string)) [10] {
+    string arr[10]; // arr is an array of ten ints
+    string *p1[10]; // p1 is an array of ten pointers
+    string (*p2)[10] = &arr; // p2 points to an array of ten ints
+    return p2;
+} 
 
 int main()
 {
+    int arr[10]; // arr is an array of ten ints
+    int *p1[10]; // p1 is an array of ten pointers
+    int (*p2)[10] = &arr; // p2 points to an array of ten ints
     return 0;
 }
--- C++ Primer/Chapter 6/Exercise_6.37.cc
+++ C++ Primer/Chapter 6/Exercise_6.37.cc
@@ -5,21 +5,33 @@ One should use a type alias, one should use a trailing return, and the third sho
 Which form do you prefer and why?
 */
 
-//Type (*function(parameter_list))[dimension]
-string (&hardWay(string s)) [10] {
-    
-    //return &array;
-} 
-
-
-
 #include <iostream>
 #include <vector>
 #include <string> 
 using std::cout, std::cin, std::endl, std::string,  std::vector, std::begin, std::end, std::runtime_error;
 
+string arr[10]; //Placed these here since they would be temp values within the function, but the function takes a string value not an array
+string (*p2)[10] = &arr; // Wish book had more detailed examples of function code, not sure if this how declarations need to be handled
+//Type (*function(parameter_list))[dimension]
+string (*hardWay(string)) [10] {
+    return p2;
+} 
+
+//decltype method
+string cold[] = {"snow", "ice", "sleet"};
+string hot[] = {"sand", "desert", "sandstorm"};
+decltype(cold) *deType(string s)
+{
+    return  s == *(&cold[1]) ? &cold : &hot; 
+}
+
+auto trailReturn(string i) -> string(*)[10];
+
 int main()
 {
-
+    string arr[10];
+    auto hdarr = hardWay("test");
+    auto dtarr = decType("test");
+    auto hdarr = hardWay("test");
     return 0;
 }
